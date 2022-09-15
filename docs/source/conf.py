f0c090a58ab75a4977df13ef9b44bf77d4537ef8
datakund <54356773+datakund@users.noreply.github.com>
2022-09-15 17:43:46 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,7 @@ exclude_patterns = []
 #
 html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
-    'collapse_navigation': False
+    'collapse_navigation': True
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
